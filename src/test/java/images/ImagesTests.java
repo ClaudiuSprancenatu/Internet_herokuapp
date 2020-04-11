6fc7f956e0dec52837dcef0bd1a52ddb4284ab78
Claudiu <sprancenatuc@yahoo.com>
2020-04-11 23:09:45 +0300

    First commit

--- src/test/java/images/ImagesTests.java
+++ src/test/java/images/ImagesTests.java
@@ -2,17 +2,19 @@ package images;
 
 import base.BaseTests;
 import org.testng.annotations.Test;
+import pages.BasePage;
 import pages.BrokenImagesPage;
 import pages.HomePage;
 
 public class ImagesTests extends BaseTests {
     private HomePage homePage;
+    private BasePage basePage;
     private BrokenImagesPage brokenImagesPage;
 
     @Test
     public void validateImages(){
         //homePage = pages.getHomePage();
-
+        basePage.visit("http://the-internet.herokuapp.com/broken_images");
         homePage.clickBrokenImage();
 
         //brokenImagesPage = pages.getBrokenImagesPage();
